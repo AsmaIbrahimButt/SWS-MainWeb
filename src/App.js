3f2c1a12f2ee25ef5d83a72082ee0660c978cba9
Asma Butt <89297894+AsmaIbrahimButt@users.noreply.github.com>
2022-10-01 00:54:51 -0700

    sws-main

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
+
+
+export default function App() {
+  return (
+   <>
+
+ 
+  <Home/>
+  
+   </>
+  )
+}
